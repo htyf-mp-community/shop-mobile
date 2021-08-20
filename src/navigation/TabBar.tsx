073cf480491a80673859a57bd4016eccd4d8023b
dmqq <kozakdamian73@gmail.com>
2021-08-20 13:37:15 +0200

    navigation bar fix

--- src/navigation/TabBar.tsx
+++ src/navigation/TabBar.tsx
@@ -1,13 +1,48 @@
 import React from "react";
-import { View, StyleSheet, Dimensions, Image } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import { Colors } from "../constants/styles";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("screen");
+
+interface OptionProps {
+  name: string;
+  text: string;
+  route: string;
+  navigation: any;
+}
+
+const Option = ({ name, text, route, navigation }: OptionProps) => {
+  const active = true;
+
+  return (
+    <TouchableOpacity
+      style={styles.touch}
+      onPress={() => navigation.navigate(route)}
+    >
+      <FontAwesome5 name={name} size={25} color={Colors.primary100} />
+      <Text style={{ color: "white" }}>{active ? text : ""}</Text>
+    </TouchableOpacity>
+  );
+};
 
 export default function TabBar({ state, descriptors, navigation }: any) {
   return (
     <View style={styles.container}>
-      <Image source={require("../assets/home.png")} style={styles.img} />
+      <Option name="home" text="home" navigation={navigation} route="Home" />
+      <Option
+        name="shopping-basket"
+        text="cart"
+        navigation={navigation}
+        route="Cart"
+      />
+      <Option name="user" text="user" navigation={navigation} route="User" />
     </View>
   );
 }
@@ -16,13 +51,12 @@ const styles = StyleSheet.create({
   container: {
     width: SCREEN_WIDTH,
     height: 60,
-    backgroundColor: Colors.primary,
-    borderColor: "#000",
-    borderWidth: 0.5,
+    backgroundColor: "#2b2b2b",
     alignItems: "center",
-    justifyContent: "center",
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
-  img: {
-    transform: [{ scale: 0.6 }],
+  touch: {
+    alignItems: "center",
   },
 });
