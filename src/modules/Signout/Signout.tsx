fd7bba748e6c4b659a762d15ec62fa05ce9c3832
dmqq <kozakdamian73@gmail.com>
2021-09-27 20:58:27 +0200

    make signout button double tap

--- src/modules/Signout/Signout.tsx
+++ src/modules/Signout/Signout.tsx
@@ -1,17 +1,22 @@
 import Button from "../../components/Button/Button";
-import React from "react";
+import React, { useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { USER_PREFIX, useUser, init } from "../../context/UserContext";
 
 export default function SignOut() {
   const { setUser } = useUser();
 
+  const [tapped, setTapped] = useState(0);
+
   async function SignOut() {
-    try {
-      await AsyncStorage.removeItem(USER_PREFIX);
-      setUser(init);
-    } catch (error) {
-      console.log(error);
+    setTapped(tapped + 1);
+    if (tapped > 1) {
+      try {
+        await AsyncStorage.removeItem(USER_PREFIX);
+        setUser(init);
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 
