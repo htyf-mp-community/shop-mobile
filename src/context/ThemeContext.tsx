4754737ddbb1cc567a130f34629f5aa691f7ff33
dmqq <kozakdamian73@gmail.com>
2021-12-04 13:29:07 +0100

    feat: added theming boilerplate

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, useContext, useState } from "react";
+import { Colors } from "../constants/styles";
+
+import { Appearance } from "react-native";
+
+interface ThemeContextType {
+  theme: typeof Colors;
+}
+
+const ThemeContext = createContext<ThemeContextType>({
+  theme: Colors,
+});
+
+export default function useTheme(): ThemeContextType {
+  return useContext(ThemeContext);
+}
+
+const THEMES = {
+  dark: Colors,
+  light: Colors,
+};
+
+export const ThemeContextProvider: React.FC = ({ children }) => {
+  const colorScheme = Appearance.getColorScheme();
+  const [theme, setTheme] = useState(THEMES[colorScheme ?? "dark"]);
+
+  console.log({ colorScheme });
+
+  return (
+    <ThemeContext.Provider value={{ theme }}>{children}</ThemeContext.Provider>
+  );
+};
