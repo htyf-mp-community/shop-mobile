c5454660f3b60fa07aceca27fd18c822cd2bcba4
DMQQ <kozakdamian73@gmail.com>
2024-03-31 10:36:45 +0200

    FIX: removed outdated tests

--- src/__tests__/Cart/CartScreen.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "@testing-library/jest-native/extend-expect";
-import { render, waitFor } from "@testing-library/react-native";
-import Cart from "../../navigation/Screens/Cart/index";
-
-describe("Cart Screen", () => {
-  test("", () => {});
-});
